5f2b7fa1b74aa624224bd74ee6a028fd038d0c11
Gioacchino Castorio <gioacchino.castorio@gmail.com>
2024-12-15 14:12:01 +0000

    ADD magit package for git frontend

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(multi-vterm timu-macos-theme elfeed ox-clip counsel ace-window which-key try org-bullets)))
+   '(magit multi-vterm timu-macos-theme elfeed ox-clip counsel ace-window which-key try org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -113,6 +113,10 @@
   (elfeed-feeds
    '("https://www.reddit.com/r/StallmanWasRight.rss")))
 
+
+;; programming tools
+(use-package magit
+  :ensure t)
 
 ;; theme
 (use-package timu-macos-theme
