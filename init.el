66707e1c948aa6666b28bb74c8ecf83357c1fe06
Gioacchino Castorio <gioacchino.castorio@gmail.com>
2024-12-23 00:10:00 +0000

    ADD ranger
    not associated with key combo; not  sure if I'll use over ssh/dired

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(move-text indent-guide focus speed-type timu-macos-theme elfeed ox-clip counsel ace-window which-key try org-bullets)))
+   '(ranger move-text indent-guide focus speed-type timu-macos-theme elfeed ox-clip counsel ace-window which-key try org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -156,6 +156,10 @@
   :ensure t
   :config (move-text-default-bindings))
 
+
+;; -------- FILE NAVIGATION ----------
+(use-package ranger
+  :ensure t)
 
 ;; -------- RANDOM ----------
 
